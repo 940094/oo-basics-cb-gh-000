e8d8a8210619faef07aa8f01a1f6ae80b50dd6ef
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 18:30:55 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,11 @@
 class Book
-  attr_accessor :title, :author, :page_count, :genre, :turn_page
+  attr_accessor :title, :author, :page_count, :genre
 
   def initialize(title)
     @title = title
   end
+
+  def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
 end
